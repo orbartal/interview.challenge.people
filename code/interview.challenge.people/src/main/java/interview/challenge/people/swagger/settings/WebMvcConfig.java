9410d0a9011e5bb18496950321bf420ebb06d405
orbartal <orbartal@gmail.com>
2019-01-16 03:05:06 -0500

    Add swagger settings.

---
+++ code/interview.challenge.people/src/main/java/interview/challenge/people/swagger/settings/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package interview.challenge.people.swagger.settings;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// Enable swagger-ui.html
+		registry.addResourceHandler("**/**").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+}
