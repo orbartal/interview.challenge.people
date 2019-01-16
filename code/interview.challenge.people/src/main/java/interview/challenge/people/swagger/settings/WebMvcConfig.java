81eea4a8ed2ad71f9c09e2a87b083ed17d818fcc
orbartal <orbartal@gmail.com>
2019-01-16 16:15:17 -0500

    Add index.html and map root url to it.

--- code/interview.challenge.people/src/main/java/interview/challenge/people/swagger/settings/WebMvcConfig.java
+++ code/interview.challenge.people/src/main/java/interview/challenge/people/swagger/settings/WebMvcConfig.java
@@ -3,14 +3,21 @@ package interview.challenge.people.swagger.settings;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+	
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/index.html").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+
 		// Enable swagger-ui.html
 		registry.addResourceHandler("**/**").addResourceLocations("classpath:/META-INF/resources/");
 
@@ -18,4 +25,9 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
+	
+	@Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "index.html");
+    }
 }
