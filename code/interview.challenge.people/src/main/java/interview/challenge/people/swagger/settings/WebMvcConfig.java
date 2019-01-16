18613e9013e2357895f60be3148f67fe3dd52867
orbartal <orbartal@gmail.com>
2019-01-16 17:10:37 -0500

    Enable js.

--- code/interview.challenge.people/src/main/java/interview/challenge/people/swagger/settings/WebMvcConfig.java
+++ code/interview.challenge.people/src/main/java/interview/challenge/people/swagger/settings/WebMvcConfig.java
@@ -17,12 +17,11 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/index.html").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+		registry.addResourceHandler("/app/**").addResourceLocations("classpath:/static/app/");
 
 		// Enable swagger-ui.html
 		registry.addResourceHandler("**/**").addResourceLocations("classpath:/META-INF/resources/");
-
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 	
