4322e2b30917502030cacfd2f3494fa67db95c23
orbartal <orbartal@gmail.com>
2019-01-16 02:41:48 -0500

    Add main that runs the spring boot server.

---
+++ code/interview.challenge.people/src/main/java/interview/challenge/people/PersonMain.java
@@ -0,0 +1,14 @@
+package interview.challenge.people;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonMain {
+
+	public static void main(String[] args) {
+		System.out.println("PetStoreMain");
+		SpringApplication.run(PersonMain.class, args);
+	}
+
+}
